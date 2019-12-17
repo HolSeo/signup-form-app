f2b0b8a7e4f6faa2c989f7d13d8219ebea8d5f69
HolSeo <dseo1004@gmail.com>
2019-12-16 22:33:57 -0500

    Always capitalize first letter of firstName

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -5,7 +5,7 @@ const Confirmation = ({ firstName, email }) => {
     return (
         <div className="Confirmation">
             <h2>Welcome</h2>
-            <h1>{firstName}!</h1>
+            <h1>{firstName.slice(0, 1).toUpperCase() + firstName.slice(1, firstName.length)}!</h1>
             <p>You have been registered for this awesome service. <br /> Please check your email listed below for instructions.</p>
             <p className="Confirmation__email">{email}</p>
             <SignInButton />
